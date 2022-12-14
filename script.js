797041db9bb6bb3ae56a1afcd437c31acded70d0
Solomon Oluwatomisola <oluwatomisola@ext.kobo360.com>
2022-12-14 17:23:07 +0100

    section 2 complete

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
+// navbar
 const menu = document.querySelector('.menu')
 const navbar = document.querySelector('.navbar')
 
 menu.addEventListener('click', () =>{
     navbar.classList.toggle('change')
     menu.classList.toggle('change')
+})
+
+// section 2 video
+const video = document.querySelector('.video')
+const button = document.querySelector('.buttons i')
+const bar = document.querySelector('.video-bar')
+
+
+button.addEventListener('click', () =>{
+    playPause()
+})
+
+const playPause = () => {
+    if (video.paused) {
+        video.play()
+        button.className = "fa-solid fa-circle-pause"
+        video.style.opacity = '.7'
+    } else {
+        video.pause()
+        button.className = "fa-solid fa-circle-play"
+        video.style.opacity = '.3'
+    }
+}
+
+video.addEventListener('timeupdate', () => {
+    const barWidth = video.currentTime/video.duration
+    bar.style.width = `${barWidth * 100}%`
+    if (video.ended) {
+        button.className = "fa-solid fa-circle-play"
+        video.style.opacity = '.3'
+    }
 })
